e529e7b019dd802b7e510173f7f43909ba8b480a
dieguitozz <diegolenda2006@gmail.com>
2024-01-22 20:06:13 -0300

    tudo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import logo from './image2.svg';
+import hello from "./image1.svg"
+import gay from "./gay.svg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+      <img src={logo}/>
+      <h1>
+        Viana Kitty
+      </h1>
       </header>
+      <div className='hellokitty'>
+        <div>
+          <img className="kitty" src={hello}/>
+          <img className="gay" src={gay}/>
+
+        </div>
+      </div>
     </div>
   );
 }
